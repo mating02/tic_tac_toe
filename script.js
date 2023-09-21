f46cf10b3a25d7a4d113039d8bad657197acd1b3
mat <mat@MyDevice>
2023-09-21 04:49:08 +0200

    added minmax function, completed layout, finished project

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
+const grid = document.querySelector('.gridBoard');
+const displayText = document.querySelector('.resultText');
+const restartBtn = document.querySelector('.btn-primary');
+const optionPlayerX = document.querySelector('.option.X');
+const optionPlayerO = document.querySelector('.option.O');
+const selectElement = document.getElementById('selectDiff');
+let selectedValue = selectElement.value; 
+let isGameRestarted = false;
+let isPlayerO = false;
+let easy = selectedValue==="easy" || selectedValue==="medium" || selectedValue==="hard";
+
 const gameBoard = (() => {
     const field = Array.from({ length: 3 }, () => new Array(3));
-    return { field };
-})();
-const Player = (marker) => {
-    const Tick = (posx, posy) => {
-        if (gameBoard.field[posx][posy] === undefined) {
-            gameBoard.field[posx][posy] = marker;
-        }
-        if (marker === "X") {
-            marker = "O";
-        }
-        else {
-            marker = "X";
+    const cleanField = () => {
+        field.forEach(row => row.fill(undefined))
+    }
+    const gridCreate = () => {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                const cell = document.createElement('div');
+                cell.classList.add('cell', `cell-${i}-${j}`);
+                grid.appendChild(cell);
+            }
         }
     }
-    const getMarker = () => marker;
-    return { Tick, getMarker };
-};
+    return { field, cleanField, gridCreate };
+})();
 
-const displayText = document.querySelector('.resultText');
+gameBoard.gridCreate();
 
 const isGameWon = () => {
     // add const winner
@@ -58,7 +66,7 @@ const PlayGame = (() => {
         let isFull = true;
         outer: for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
-                if (gameBoard.field[i][j] !== undefined) {
+                if (gameBoard.field[i][j] === undefined) {
                     isFull = false;
                     break outer;
                 }
@@ -70,50 +78,250 @@ const PlayGame = (() => {
     const displayStatus = (player) => {
         const gameWon = isGameWon();
         const boardFull = isBoardFull();
-        if (gameWon.value || boardFull.isFull) {
-            if (gameWon.value) {
-                displayText.textContent = `${gameWon.winner} has won the game!`;
-            }
-            else {
-                displayText.textContent = "It's a draw!";
-            }
+        if (gameWon.value) {
+            displayText.textContent = `${gameWon.winner} has won the game!`;
         }
-        else{
-            displayText.textContent = `${player.getMarker()}'s turn!`;
+        else if (boardFull.isFull) {
+            displayText.textContent = "It's a draw!";
+        }
+        else {
+            //if playerSelection === "X"...
+            displayText.textContent = `Let's go!`;
         }
     }
-    return { displayStatus };
+    return { displayStatus, isBoardFull };
 })();
 
-const grid = document.querySelector('.gridBoard');
+const Player = (marker) => {
+    const Tick = (posx, posy) => {
+        if (gameBoard.field[posx][posy] === undefined) {
+            gameBoard.field[posx][posy] = marker;
+        }
+        const checkResult = isGameWon();
+        if (marker === "X") {
+            marker = "O";
+            if (!checkResult.value && !PlayGame.isBoardFull().isFull) {
+                if(easy){
+                    Bot().playRandom(marker);
+                }
+                else{
+                    Bot().playHard(marker);
+                }
+                marker = "X";
+            }
+
+        }
+        else {
+            marker = "X";
+            if (!checkResult.value && !PlayGame.isBoardFull().isFull) {
+                if(easy){
+                    Bot().playRandom(marker);
+                }
+                else{
+                    Bot().playHard(marker);
+                }
+                marker = "O";
+            }
+        }
+    }
+    const getMarker = () => marker;
+    const setMarker = (mark) => marker = mark;
+    return { Tick, getMarker, setMarker };
+};
+
+const fieldSteps = Array.from({length: 3}, () => new Array(3).fill(0)); // This array is necessary to find the best move
+
+function minmax(marker, player, steps){
+    const gameWon = isGameWon();
+    if(gameWon.value || PlayGame.isBoardFull().isFull){
+        if(gameWon.winner === marker){
+            return 100/steps;     //so the maximum will be the min number of steps
+        }
+        else if(gameWon.winner !== marker){
+            return -100 /steps;  //so the maximum will be the min number of steps
+        }
+        else{
+            return 0;
+        }
+    }
+    if(player.getMarker() === marker){
+        let value = Number.MIN_SAFE_INTEGER;
+        let bestMove = { row: -1, col: -1};
+        for(let i = 0; i < 3; i++){
+            for(let j= 0; j < 3; j++){
+                if(gameBoard.field[i][j] === undefined){
+                    gameBoard.field[i][j] = player.getMarker();
+                    const opponentMarker = player.getMarker() === "X" ? "O" : "X";
+                    player.setMarker(opponentMarker);
+                    const newVal = minmax(marker, player, steps+1);
+                    if(newVal > value){
+                        value = newVal;
+                        bestMove = { row: i, col: j };
+                    }
+                    gameBoard.field[i][j] = undefined;
+                    fieldSteps[i][j] = value;
+                    const originalMarker = player.getMarker() === "X" ? "O" : "X";
+                    player.setMarker(originalMarker);
+                }
+            }
+        }
+        if (steps === 0) {
+            // If it's the top-level call, return the best move
+            return bestMove;
+        }
+        return value;
+    }
+    else{
+        let val = Number.MAX_SAFE_INTEGER;
+        let bestMove = { row:-1, col:-1 };
+        for(let i = 0; i < 3; i++){
+            for(let j= 0; j < 3; j++){
+                if(gameBoard.field[i][j] === undefined){
+                    gameBoard.field[i][j] = player.getMarker();
+                    const opponentMarker = player.getMarker() === "X" ? "O" : "X";
+                    player.setMarker(opponentMarker);
+                    const newVal = minmax(marker, player, steps+1);
+                    if(newVal < val){
+                        val = newVal;
+                        bestMove = { row: i, col: j };
+                    }
+                    gameBoard.field[i][j] = undefined;
+                    fieldSteps[i][j] = val;
+                    const originalMarker = player.getMarker() === "X" ? "O" : "X";
+                    player.setMarker(originalMarker);
+                }
+            }
+        }
+        if (steps === 0) {
+            // If it's the top-level call, return the best move
+            return bestMove;
+        }
+        return val;
+    }
+}
+
+//Ort der besten Wahrscheinlichkeit speichern
+
+const Bot = (() => {
+    const playRandom = (marker) => {
+        let randomRow = Math.floor(Math.random() * 3);
+        let randomCol = Math.floor(Math.random() * 3);
+        while (gameBoard.field[randomRow][randomCol] !== undefined) {
+            randomRow = Math.floor(Math.random() * 3);
+            randomCol = Math.floor(Math.random() * 3);
+        }
+        const chosenCell = document.querySelector(`.cell-${randomRow}-${randomCol}`);
+        gameBoard.field[randomRow][randomCol] = marker;
+        if (marker === "X") {
+            chosenCell.style.color = "#FF5722";
+        }
+        else {
+            chosenCell.style.color = "#2196F3";
+        }
+        chosenCell.textContent = marker;
+    };
+        const playHard = (marker) => {
+        const currentPlayer = Player(marker);
+        const bestMove = minmax(marker, currentPlayer, 0);
+        const chosenCell = document.querySelector(`.cell-${bestMove.row}-${bestMove.col}`);
+        gameBoard.field[bestMove.row][bestMove.col] = marker;
+        if (marker === "X") {
+            chosenCell.style.color = "#FF5722";
+        }
+        else {
+            chosenCell.style.color = "#2196F3";
+        }
+        chosenCell.textContent = marker;
+    }
+
+    return { playRandom, playHard};
+});
+
 const displayController = (() => {
-    const currentPlayer = Player("X");
-    const gridCreate = () => {
+    let currentPlayer = Player("X");
+    const activateBoard = () => {
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
-                const cell = document.createElement('div');
+              
+                const cell = document.querySelector(`.cell-${i}-${j}`);
                 cell.addEventListener('click', () => {
                     if (gameBoard.field[i][j] === undefined) {
                         const checkGame = isGameWon();
                         if (!checkGame.value) {
+                            if(isPlayerO){
+                                currentPlayer.setMarker("O");
+                            }
+                            else{
+                                currentPlayer.setMarker("X");
+                            }
+                            if (currentPlayer.getMarker() === "X") {
+                                cell.style.color = "#FF5722";
+                            }
+                            else {
+                                cell.style.color = "#2196F3";
+                            }
                             cell.textContent = currentPlayer.getMarker();
                             currentPlayer.Tick(i, j);
-                            PlayGame.displayStatus(currentPlayer);
                         }
                     }
-                })
-                cell.classList.add('cell');
-                grid.appendChild(cell);
-            }
+                    PlayGame.displayStatus(currentPlayer);
+                });
+            };
+        };
+    };
+    return { activateBoard };
+});
+
+
+function restartGame() {
+    gameBoard.cleanField();
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        cell.textContent = '';
+    });
+    if(isPlayerO){
+        if(easy){
+            Bot().playRandom("X");
+        }
+        else{
+            Bot().playHard("X");
         }
     }
-    return { gridCreate };
-})();
+    isGameRestarted = true;
+    displayText.textContent = "Let's go!";
+}
 
-const ChooseRandom = (player) => {
-    let randomRow = Math.floor(Math.random() * 3);
-    let randomCol = Math.floor(Math.random() * 3);
-    player.Tick(randomCol, randomRow);
-};
+restartBtn.addEventListener('click', () => {
+    restartGame();
+});
 
-displayController.gridCreate();
+optionPlayerO.addEventListener('click', () => {
+    let checkPlayerO = true;
+    if(!isPlayerO){
+        checkPlayerO = false;
+    }
+    isPlayerO = true;
+    if(!checkPlayerO){restartGame();}
+    optionPlayerO.style.border = '3px solid black';
+    optionPlayerX.style.border = '1px solid black';
+});
+
+optionPlayerX.addEventListener('click', () => {
+    let checkPlayerO = true;
+    if(!isPlayerO){
+        checkPlayerO = false;
+    }
+    isPlayerO = false;
+    if(checkPlayerO){
+        restartGame();
+    }
+    optionPlayerO.style.border = '1px solid black';
+    optionPlayerX.style.border = '3px solid black';
+});
+
+selectElement.addEventListener('change', (event) => {
+    restartGame();
+    easy = event.target.value === "easy" || event.target.value === "medium" || event.target.value === "hard";
+});
+
+displayController().activateBoard();
